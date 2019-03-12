f0cc33439b25a978d5f64f44e70003efdc41b063
teddy meksavanh <teddymeksavanh@yahoo.fr>
2019-03-12 12:08:28 +0100

    Add 404 page, and rectify lazy load on team

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 // Services
 import React, { Component } from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import {
+  Switch,
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
 
 // Components
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-// import Whitepaper from './components/Whitepaper';
 import Footer from './components/Footer';
 import Team from './components/Team';
 import Contact from './components/Contact';
@@ -14,6 +17,7 @@ import Ecosystem from './components/Ecosystem';
 import Blockchain from './components/Blockchain';
 import Privacy from './components/Privacy';
 import Cookie from './components/Cookie';
+import Page404 from './components/page404';
 
 import './App.css';
 
@@ -34,19 +38,23 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="App">
-        <Navbar/>
-        <Route exact path="/" component={props => <Home/>}/>
-        <Route exact path="/contact" component={props => <Contact/>}/>
-        <Route exact path="/device" component={props => <Device/>}/>
-        <Route exact path="/ecosystem" component={props => <Ecosystem/>}/>
-        <Route exact path="/blockchain" component={props => <Blockchain/>}/>
-        <Route exact path="/privacy" component={props => <Privacy/>}/>
-        <Route exact path="/cookie" component={props => <Cookie/>}/>
-        {/* <Route exact path="/whitepaper" component={props => <Whitepaper/>}/> */}
-        <Route exact path="/team" component={props => <Team/>}/>
-        <Footer/>
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar/>
+          <Switch>
+            <Route exact path="/" component={props => <Home/>}/>
+            <Route exact path="/contact" component={props => <Contact/>}/>
+            <Route exact path="/device" component={props => <Device/>}/>
+            <Route exact path="/ecosystem" component={props => <Ecosystem/>}/>
+            <Route exact path="/blockchain" component={props => <Blockchain/>}/>
+            <Route exact path="/privacy" component={props => <Privacy/>}/>
+            <Route exact path="/cookie" component={props => <Cookie/>}/>
+            <Route exact path="/team" component={props => <Team/>}/>
+            <Route component={props => <Page404 />} />
+          </Switch>
+          <Footer/>
+        </div>
+      </Router>
     );
   }
 }
